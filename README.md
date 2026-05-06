2c6936550df595fbf4a7605079ea49fd2c5c85b1
Léo Imbert <leoimbert5210@gmail.com>
2026-05-06 11:10:11 +0200

    Distance

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# peip1-projet
+# Geometric Drawing Project
 
+This project is made in C++.  
+It gives simple shapes to draw on the screen
+
+## Square
+
+## Circle
+
+## Triangle
--- src/point.cpp
+++ src/point.cpp
@@ -1,3 +1,9 @@
 #include "point.hpp"
+#include <cmath>
 
 Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+
+double Point::distance(Point B){
+    double X = (x - B.x), Y = (y - B.y)
+    return sqrt(X * X + Y * Y)
+}
