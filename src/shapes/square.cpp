ac67ae1e94358c4fc9dcaaa465abe9fb5b50683e
matsilo2000 <2000M@tsilo>
2026-05-06 11:50:37 +0200

    Carre

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,24 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
+#include <iostream> 
+#include <cmath>
 
 Square::Square(Point P, Point R) : A(P), C(R) {};
+
+Square::double side(){
+    return A.x - C.x;
+}
+
+Square::double perimeter(){
+    return 4*(A.x-C.x);
+
+}
+
+Square::double area(){
+    return (A.x-C.x)*(A.x-C.x);
+}
+
+Square::void draw(){
+    for (int i=0; i<A.x-C.x; i++){
+        cout << "*";}
+}
