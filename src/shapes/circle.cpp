34978ba098804f6ebe9093078b5a2f4171738a1f
RodyMN <rodrigo.manatas-nascimento@universite-paris-saclay.fr>
2026-05-06 11:03:22 +0200

    test du commit et ajout de la fonction circumference

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -1,4 +1,12 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
+double circumference(){
+  return 2*M_PI*radius ;
+  
+}
+
