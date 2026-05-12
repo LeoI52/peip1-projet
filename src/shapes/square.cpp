278496e26de5e8d84b1a6f017d035fe5bf7bae5c
Léo Imbert <leoimbert5210@gmail.com>
2026-05-12 15:11:03 +0200

    Update square.cpp

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,94 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
+#include "draw.hpp"
+#include <iostream> 
+#include <cmath>
+#include <vector>
+using namespace std;
+
 
 Square::Square(Point P, Point R) : A(P), C(R) {};
+
+double Square::side() {
+    double diag = A.distance(C);
+    return diag / std::sqrt(2.0);
+}
+
+double Square::perimeter(){
+    return 4*side();
+
+}
+
+double Square::area() {
+    double s = side();
+    return s * s;
+}
+
+Point Square::center() {
+    return Point((A.x + C.x) / 2.0, (A.y + C.y) / 2.0);
+}
+
+void Square::draw(){
+    std::vector<Point> Square={A,C};
+    draw_picture (Square);
+}
+
+
+void Square::translate(Point T){
+    A.x=A.x+T.x;
+    A.y=A.y+T.y;
+
+
+}
+
+void Square::resize(double ratio){
+    Point O=center();
+    A.x=O.x +(A.x-O.x)*ratio;
+    A.y=O.y+(A.y-O.y)*ratio;
+    C.x=O.x +(C.x-O.x)*ratio;
+    C.y=O.y+(C.y-O.y)*ratio;
+}
+
+
+
+void Square::rotate(double angle) {
+
+    Point O= center();
+    // calcul de chaque écart entre le centre et le point
+    double x1 = A.x - O.x; 
+    double y1 = A.y - O.y;
+    double x2 = C.x - O.x;
+    double y2 = C.y - O.y;
+
+    double cosA = std::cos(angle); //cosinus de l angle
+    double sinA = std::sin(angle);// sinus de l angle
+
+
+    // calcule les coordonnées des points avec rotation 
+    double rx1 = x1 * cosA - y1 * sinA; //r..=rotation
+    double ry1 = x1 * sinA + y1 * cosA;
+
+    double rx2 = x2 * cosA - y2 * sinA;
+    double ry2 = x2 * sinA + y2 * cosA;
+
+    // calcule coordonnées point finaux par rapport au centre
+    A.x = O.x + rx1;
+    A.y = O.y + ry1;
+
+    C.x = O.x + rx2;
+    C.y = O.y + ry2;
+}
+
+bool Square::equals(Square square){
+    return square.area()==area() && square.perimeter()==perimeter();
+ }
+
+Circle Square::inscribedCircle() {
+    // Le rayon est la moitié du côté
+    return Circle(side()/2.0, center());
+}
+
+Circle Square::circumscribedCircle() {
+    // Le rayon est la moitié de la diagonale
+    return Circle(A.distance(C) / 2.0, center());
+}
